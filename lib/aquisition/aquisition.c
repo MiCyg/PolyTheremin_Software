88076d5acdab6ca3d894457e21c06330c9fb3f17
Mi <68618203+MiCyg@users.noreply.github.com>
2025-02-04 20:05:40 +0100

    feat: Add aquisition component

---
+++ lib/aquisition/aquisition.c
@@ -0,0 +1,20 @@
+#include "aquisition.h"
+
+
+
+
+
+void aquisition_init()
+{
+	logg(FREQ_DET, "Aquisition init\n");
+	// 
+
+
+
+
+}
+
+void aquisition_deinit()
+{
+	logg(FREQ_DET, "Aquisition deinit\n");
+}
---
+++ lib/aquisition/aquisition.h
@@ -0,0 +1,13 @@
+#ifndef __AQUISITION__
+#define __AQUISITION__
+
+#include "comlib.h"
+
+
+
+void aquisition_init();
+void aquisition_deinit();
+
+
+
+#endif // __AQUISITION__

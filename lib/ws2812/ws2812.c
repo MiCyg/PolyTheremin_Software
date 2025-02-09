6cdc443f1eff5c0f498c2334850cf0d4ba2e1f70
Mi <68618203+MiCyg@users.noreply.github.com>
2025-02-09 21:37:40 +0100

    refactor: Use proper names

--- lib/ws2812/ws2812.c
+++ lib/ws2812/ws2812.c
@@ -3,13 +3,16 @@
 #include "ws2812.pio.h"
 #include "hardware/clocks.h"
 
-
+static PIO ws2812_pio = WS2812_PIO_INSTANCE;
+static uint ws2812_offset;
+static int ws2812_sm;
 
 
 static inline void ws2812_program_init(PIO pio, uint sm, uint offset, uint pin, float freq, bool rgbw) {
-
+	int err;
     pio_gpio_init(pio, pin);
-    pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
+    err = pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
+	panic_check(err != 0, "Cannot set pindirs");
 
     pio_sm_config c = ws2812_program_get_default_config(offset);
     sm_config_set_sideset_pins(&c, pin);
@@ -20,7 +23,8 @@ static inline void ws2812_program_init(PIO pio, uint sm, uint offset, uint pin, 
     float div = clock_get_hz(clk_sys) / (freq * cycles_per_bit);
     sm_config_set_clkdiv(&c, div);
 
-    pio_sm_init(pio, sm, offset, &c);
+    err = pio_sm_init(pio, sm, offset, &c);
+	panic_check(err != 0, "Cannot init state machine");
     pio_sm_set_enabled(pio, sm, true);
 }
 
@@ -32,28 +36,27 @@ static inline void ws2812_program_deinit(PIO pio, uint sm, uint offset) {
 
 // ======================= EXTERNAL =======================
 
-PIO pio;
-uint offset;
-int sm = 0;
 int ws2812_init(){
-	pio = WS2812_PIO_INSTANCE;
 
-	// sm = pio_claim_unused_sm(pio,false);
-	// if(sm == -1) return -1;
+	ws2812_sm = pio_claim_unused_sm(ws2812_pio,false);
+	panic_check(ws2812_sm == -1, "Cannot claim unused sm");
 
-    offset = pio_add_program(pio, &ws2812_program);
-    ws2812_program_init(pio, sm, offset, GPIO_UI_LED, WS2812_SPEED, WS2812_IS_RGBW);
+    ws2812_offset = pio_add_program(ws2812_pio, &ws2812_program);
+	panic_check(ws2812_sm == -1, "Cannot add program");
+
+	logg(LED, "Claim sm: %d, offset: %d\n", ws2812_sm, ws2812_offset);
+    ws2812_program_init(ws2812_pio, ws2812_sm, ws2812_offset, GPIO_UI_LED, WS2812_SPEED, WS2812_IS_RGBW);
 	return 0;
 }
 
 int ws2812_deinit(){
-	ws2812_program_deinit(pio, sm, offset);
-	pio_remove_program(pio, &ws2812_program, offset);
+	ws2812_program_deinit(ws2812_pio, ws2812_sm, ws2812_offset);
+	pio_remove_program(ws2812_pio, &ws2812_program, ws2812_offset);
 	return 0;
 }
 
 void ws2812_put(color_t pixel_grb){
-	pio_sm_put_blocking(pio, 0, pixel_grb.raw << 8u);
+	pio_sm_put_blocking(ws2812_pio, 0, pixel_grb.raw << 8u);
 }
 
 

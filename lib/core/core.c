bc64a65b46d1fbd7ae156fefc81cb6dacd813920
Mi <68618203+MiCyg@users.noreply.github.com>
2025-02-14 20:43:41 +0100

    refactor: Replace welcome message with neofetch display and remove unused code

--- lib/core/core.c
+++ lib/core/core.c
@@ -4,36 +4,42 @@
 #include "aquisition.h"
 #include "dds.h"
 
+#define FETCH_COLOR COLOR_CYAN
+void neofetch_message(){
+	// TODO create custom pico board and update information, simplified logo display
 
-void welcome_message(){
+	printf("\n");
+	printf(COLOR_GREEN "         ...                . .         \n" NOCOLOR                         );
+	printf(COLOR_GREEN "    ^~77??JJ??7!^.     :~77?J?J?77!^.   \n" NOCOLOR                         );
+	printf(COLOR_GREEN "   .?YYYJJJJYYYYYJ^  .?YYYYYYJJJYYYY^   \n" NOCOLOR                         );
+	printf(COLOR_GREEN "    ~YYYYJJ?77?JYYY. !5YYJ777?JYYYY7.   \n" NOCOLOR                         );
+	printf(COLOR_GREEN "     ~YYYYYYYJ7~!?~  :77~!?JYYYYYY?:    \n" NOCOLOR                         );
+	printf(COLOR_GREEN "      :7JYYYYYYJ:       7YYYYYYY?~.     " NOCOLOR "%s" NOCOLOR " %s\n", PROJECT_NAME, PROJECT_VERSION);
+	printf(COLOR_GREEN "        .^!?7!~: " COLOR_RED " .... " COLOR_GREEN " .^!777~:        \n" NOCOLOR );
+	printf(COLOR_RED "        ..:^^: .^~~!!~~^: :^^:.         " FETCH_COLOR "---------------------\n" NOCOLOR );
+	printf(COLOR_RED "      .^~!!~:  ^!!!!!!!!~..^~!!~^       " FETCH_COLOR "OS:"         NOCOLOR " freeRTOS %s\n", tskKERNEL_VERSION_NUMBER);
+	printf(COLOR_RED "      ~!~^:  ....:^^^^:::....:~!!^      " FETCH_COLOR "Board:"      NOCOLOR " %s\n",          PICO_BOARD);
+	printf(COLOR_RED "      ^:. :^~~!~~^    :~~!!~~:..:^      " FETCH_COLOR "Build type:" NOCOLOR " %s\n",          PICO_CMAKE_BUILD_TYPE);
+	printf(COLOR_RED "    :^  .~!!!!!!!!~  ^!!!!!!!!~. .^:    " FETCH_COLOR "Uptime:"     NOCOLOR " %ld[us]\n",     time_us_32());
+	printf(COLOR_RED "  .~!!: ^!!!!!!!!!~. ^!!!!!!!!!~ ^!!~.  " FETCH_COLOR "CPU:"        NOCOLOR " %s (%d)\n",     "Cortex M0+", configNUMBER_OF_CORES);
+	printf(COLOR_RED "  ~!!!: ^!!!!!!!!!:   ~!!!!!!!!~ :!!!^  " FETCH_COLOR "Sys clock:"  NOCOLOR " %.0fkHz\n",     (double)(clock_get_hz(clk_sys))/1000);
+	printf(COLOR_RED "  ^!!~.  ^~!!!!~^......:^~!!!~^.  ~!!^  " FETCH_COLOR "Flash:"      NOCOLOR " %dMB\n",        PICO_FLASH_SIZE_BYTES/1024/1024);
+	printf(COLOR_RED "  .^~.     .:...^~~!!!~^:....  ...:~^   " FETCH_COLOR "SRAM:" NOCOLOR " %dkB\n", 0); // TODO any method to show ram usage?
+	printf(COLOR_RED "     :~~~^.   .~!!!!!!!!!^   :~~!!^     \n" NOCOLOR                           );
+	printf(COLOR_RED "     ~!!!!!^. :!!!!!!!!!!~ .~!!!!!!.    \n" NOCOLOR                           );
+	printf(COLOR_RED "     :!!!!!!~. ^!!!!!!!!~..~!!!!!!^     \n" NOCOLOR                           );
+	printf(COLOR_RED "      :~!!!!!^  .:^^^^:.  ^!!!!!~:      \n" NOCOLOR                           );
+	printf(COLOR_RED "        .:^^^.  ..:::::.. .:^^:.        \n" NOCOLOR                           );
+	printf(COLOR_RED "              .~!!!!!!!!!:              \n" NOCOLOR                           );
+	printf(COLOR_RED "               .:^~~~~~^.               \n" NOCOLOR                           );
+	printf(COLOR_RED "                    .                   \n" NOCOLOR                           );
 
-
-	gpio_init(GPIO_TEST);
-	gpio_set_dir(GPIO_TEST, true);
-	gpio_put(GPIO_TEST, 0);
-	vTaskDelay(pdMS_TO_TICKS(10));
-	gpio_put(GPIO_TEST, 1);
-	vTaskDelay(pdMS_TO_TICKS(10));
-	gpio_put(GPIO_TEST, 0);
-
-
-	logc(CORE, COLOR_PURPLE, "");
-	logc(CORE, COLOR_PURPLE, "-----------------------------------------");
-	logc(CORE, COLOR_PURPLE, "|             BOOT SUCCESS!             |");
-	logc(CORE, COLOR_PURPLE, "-----------------------------------------");
-	logc(CORE, COLOR_GREEN, "Project name:   \'%s\'",   PROJECT_NAME );
-	logc(CORE, COLOR_GREEN, "Meas sys clock:  %.0fkHz",   (double)(clock_get_hz(clk_sys))/1000);
-	logc(CORE, COLOR_GREEN, "Version:         %s",      PROJECT_VERSION);
-	logc(CORE, COLOR_GREEN, "Describe:       \'%s\'",   PROJECT_VERSION_DESCRIBE);
-	logc(CORE, COLOR_GREEN, "Core cpu:        %d",   get_core_num());
 }
-
-
 
 
 void core_run()
 {
-	welcome_message();
+	neofetch_message();
 
 	led_init();
 	led_set_anim(LED_ANIM_HEART);
@@ -43,30 +49,6 @@ void core_run()
 	QueueHandle_t aquisitionDdsQueue = xQueueCreate(16, sizeof(float32_t)*CHAN_NUM);
 	aquisition_init(&aquisitionDdsQueue);
 	dds_init(&aquisitionDdsQueue);
-
-
-	dds_set_sound(0, 1);
-	dds_set_sound(1, 1);
-	dds_set_sound(2, 2);
-	dds_set_sound(3, 3);
-	
-	dds_set_amp(0, int2fix32(0));
-	dds_set_amp(1, int2fix32(1));
-	dds_set_amp(2, int2fix32(1));
-	dds_set_amp(3, int2fix32(0));
-	// dds_set_freq(0, int2fix32(990));
-
-
-	// while (1)
-	// {
-	// 	for (uint16_t _freq = 100; _freq < 5000; _freq+=1)
-	// 	{
-	// 		dds_set_freq(0, int2fix32(_freq));
-	// 		vTaskDelay(pdMS_TO_TICKS(10));
-	// 	}
-	// }
-	
-
 
 	vTaskDelete(NULL);
 }
